fcf08fdd4cb3a8a849345f46b13bde4526117248
cajabor <chinedumajabor@gmail.com>
2023-04-30 02:36:14 -0400

    moving around in dist folder

---
+++ src/dist/root.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Root() {
+  return (
+    <div>
+      <article>
+        <h3>
+          Welcome to Must Do It, the ultimate task management platform. With our easy-to-use interface,
+          you can create, organize, and prioritize your to-do list with ease. Log in or sign up to get started today!
+        </h3>
+      </article>
+    </div>
+  );
+}
+
+export default Root;
