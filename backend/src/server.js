58fa918864cc04e7d886890a5f44f25574edac9a
cajabor <chinedumajabor@gmail.com>
2023-05-01 13:54:57 -0400

    new database cluster, closing connections

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,15 +8,16 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
-const mongoURL = 'mongodb+srv://cajabor2018:NKcvTG6OokCVCkmX@cluster0.pxbkleu.mongodb.net/test';
-const client = new MongoClient(mongoURL);
-await client.connect()
+const mongoURL = 'mongodb+srv://cajabor2018:NKcvTG6OokCVCkmX@cluster0.so8lvfl.mongodb.net/test';
+
 
 
 app.get('/api/tasks/:name', async (req, res) => {
     const { name } = req.params
     console.log(name)
     
+    const client = new MongoClient(mongoURL);
+    await client.connect()
  
     const db = client.db('react-tasks-db');
 
@@ -26,11 +27,15 @@ app.get('/api/tasks/:name', async (req, res) => {
     }else{
         res.sendStatus(404);
     }
+
+    await client.close();
 });
 
 app.get('/api/tasks', async (req, res) => {
     
- 
+    const client = new MongoClient(mongoURL);
+    await client.connect()
+
     const db = client.db('react-tasks-db');
 
     const tasks = await db.collection('tasks').find({}).toArray(function(err, docs){});
@@ -39,10 +44,14 @@ app.get('/api/tasks', async (req, res) => {
     }else{
         res.sendStatus(404);
     }
+    await client.close();
 });
 
 app.put('/api/details/:name/updateStatus', async (req, res) => {
     const { name } = req.params;
+
+    const client = new MongoClient(mongoURL);
+    await client.connect()
 
     const db = client.db('react-tasks-db');
     console.log(name);
@@ -60,12 +69,16 @@ app.put('/api/details/:name/updateStatus', async (req, res) => {
             res.send(docs);
         });
     }
+
+    await client.close();
 });
 
 
 app.post('/api/createnew/', async (req, res) => {
     const { taskID, taskName, taskCategory, dueDate, status, location } = req.body;
 
+    const client = new MongoClient(mongoURL);
+    await client.connect()
 
     const db = client.db('react-tasks-db');
     const response = await db.collection('tasks').insertOne({
@@ -76,12 +89,15 @@ app.post('/api/createnew/', async (req, res) => {
         res.send(docs);
     });
  
+    await client.close();
 });
 
 app.put('/api/:name/delete', async (req, res) =>{
     
     const {name} = req.params;
     
+    const client = new MongoClient(mongoURL);
+    await client.connect()
 
     const db = client.db('react-tasks-db');
 
@@ -90,6 +106,8 @@ app.put('/api/:name/delete', async (req, res) =>{
     await db.collection('tasks').find({}).toArray(function(err, docs){
         res.send(docs);
     });
+
+    await client.close();
 });
 app.listen(8000, () => { 
     console.log('serve is listening on port 8000');
