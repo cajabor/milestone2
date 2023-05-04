33b64bdf5af4b60537fec515c49ab2b0df213cff
cajabor <chinedumajabor@gmail.com>
2023-05-04 09:19:19 -0400

    trying out potential localhost and netlify proxy

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,7 @@ app.use(express.json());
 //https://guileless-quokka-8e312d.netlify.app
 //http://localhost:3000
  app.use(function(req, res, next) {
-     res.header("Access-Control-Allow-Origin", "http://localhost:3000"); 
+     res.header("Access-Control-Allow-Origin", "http://localhost", "https://guileless-quokka-8e312d.netlify.app"); 
      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
      next();
    });
